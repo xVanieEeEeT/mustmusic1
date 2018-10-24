9a2c672b571ca689770c200fae09c7ae791767ec
xVanieEeEeT <42171095+xVanieEeEeT@users.noreply.github.com>
2018-10-24 23:06:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,6 +24,31 @@ const prefix = "1";
 /////////////////////////
 ////////////////////////
 
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    console.log('')
+    console.log('')
+    console.log('╔[════════════════════════════════════════════════════════════════]╗')
+    console.log(`[Start] ${new Date()}`);
+    console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+    console.log('')
+    console.log('╔[════════════════════════════════════]╗');
+    console.log(`Logged i as * [ " ${client.user.username} " ]`);
+    console.log('')
+    console.log('Informatins :')
+    console.log('')
+    console.log(`servers! [ " ${client.guilds.size} " ]`);
+    console.log(`Users! [ " ${client.users.size} " ]`);
+    console.log(`channels! [ " ${client.channels.size} " ]`);
+    console.log('╚[════════════════════════════════════]╝')
+    console.log('')
+    console.log('╔[════════════]╗')
+    console.log(' Bot Is Online')
+    console.log('╚[════════════]╝')
+    console.log('')
+    console.log('')
+  });
+
 
 client.on('message', message => {
   var args = message.content.split(' ').slice(1);
